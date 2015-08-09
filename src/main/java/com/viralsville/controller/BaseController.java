5de5ed8953243b8aa818b174b5ecddbd64cc7a2b
Tom Forzaglia <thomas.forzaglia@gmail.com>
2015-08-08 23:04:15 -0400

    add spring web mvc

--- src/main/java/com/viralsville/controller/BaseController.java
+++ src/main/java/com/viralsville/controller/BaseController.java
@@ -1,13 +1,20 @@
 package com.viralsville.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class BaseController {
 
     @RequestMapping("/")
+    @ResponseBody
     public String index() {
         return "This is Viralsville!";
+    }
+
+    @RequestMapping("/home")
+    public String home() {
+        return "index";
     }
 }
