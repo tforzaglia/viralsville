256a13a04270ec6cdd5c96d3e08acbbef1faafc1
Tom Forzaglia <thomas.forzaglia@gmail.com>
2015-08-26 23:10:13 -0400

    set up base controller method to pass content list as model attribute

--- src/main/java/com/viralsville/controller/BaseController.java
+++ src/main/java/com/viralsville/controller/BaseController.java
@@ -1,20 +1,33 @@
 package com.viralsville.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.viralsville.data.ContentRepository;
+import com.viralsville.model.Content;
 
 @Controller
 public class BaseController {
 
-    @RequestMapping("/test")
-    @ResponseBody
-    public String test() {
-        return "This is Viralsville!";
-    }
+    @Autowired
+    private ContentRepository contentRepository;
 
     @RequestMapping("/")
-    public String index() {
+    public String index( Model model ) {
+        List<Content> contents = this.contentRepository.getContentListByPageNumber( 1 );
+        model.addAttribute( "contents", contents );
+        return "redirect:/content?page=1";
+    }
+
+    @RequestMapping("/content")
+    public String content( @RequestParam("page") int pageNumber, Model model ) {
+        List<Content> contents = this.contentRepository.getContentListByPageNumber( pageNumber );
+        model.addAttribute( "contents", contents );
         return "index";
     }
 }
