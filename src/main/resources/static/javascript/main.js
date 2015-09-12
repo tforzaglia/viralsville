10bc096e154bd4faf0a59e061d52a86ba9b59905
Tom Forzaglia <thomas.forzaglia@gmail.com>
2015-09-12 18:11:39 -0400

    call search when user presses enter, don't call search if search term is empty, edit the no results returned text

--- src/main/resources/static/javascript/main.js
+++ src/main/resources/static/javascript/main.js
@@ -1,5 +1,15 @@
+$(document).ready(function() {
+	document.getElementById("search-query").onkeydown = function(event) {
+		if (event.keyCode == 13) {
+			searchContent();
+		}
+	}
+});
+
 searchContent = function() {
 	var searchTerm = $("#search-query").val();
-	console.log("Hitting the search endpoint for search term " + searchTerm);
-	window.location.href = "http://localhost:8080/search?searchTerm=" + searchTerm;
-}
+	if (searchTerm !== "") {
+		console.log("Hitting the search endpoint for search term " + searchTerm);
+		window.location.href = "http://localhost:8080/search?searchTerm=" + searchTerm;
+	}
+};
