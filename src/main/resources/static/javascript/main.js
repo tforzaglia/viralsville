7489acb58c1e3adddaf01b7cb5c594600b96160b
Tom Forzaglia <thomas.forzaglia@gmail.com>
2015-09-16 21:46:08 -0400

    stop social media div from scrolling over the footer

--- src/main/resources/static/javascript/main.js
+++ src/main/resources/static/javascript/main.js
@@ -1,9 +1,43 @@
 $(document).ready(function() {
+	
+	// perform content search when user presses the enter key
 	document.getElementById("search-query").onkeydown = function(event) {
 		if (event.keyCode == 13) {
 			searchContent();
 		}
 	}
+	
+	// stop the social media from scrolling over the footer
+	$.fn.scrollBottom = function() {
+        return $(document).height() - this.scrollTop() - this.height();
+    };
+
+    var $el = $('#social-media-div');
+    var $window = $(window);
+    var top = $el.parent().position().top;
+
+    $window.bind("scroll resize", function() {
+        var gap = $window.height() - $el.height() - 10;
+        var visibleFoot = 275 - $window.scrollBottom();
+        var scrollTop = $window.scrollTop()
+
+        if (scrollTop < top + 10) {
+            $el.css({
+                top: (top - scrollTop) + "px",
+                bottom: "auto"
+            });
+        } else if (visibleFoot > gap) {
+            $el.css({
+                top: "auto",
+                bottom: visibleFoot + "px"
+            });
+        } else {
+            $el.css({
+                top: 0,
+                bottom: "auto"
+            });
+        }
+    }).scroll();
 });
 
 searchContent = function() {
