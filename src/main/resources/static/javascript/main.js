3a002bec04d1e2492b95669ce2d0dd1fcee11f77
Tom Forzaglia <thomas.forzaglia@gmail.com>
2015-10-07 22:10:15 -0400

    justify the text of the header links

--- src/main/resources/static/javascript/main.js
+++ src/main/resources/static/javascript/main.js
@@ -38,6 +38,11 @@ $(document).ready(function() {
             });
         }
     }).scroll();
+    
+    // justify the text of the header links
+    $('.stretch').each(function () {
+        $(this).strech_text();
+    });
 });
 
 searchContent = function() {
@@ -46,4 +51,29 @@ searchContent = function() {
 		console.log("Hitting the search endpoint for search term " + searchTerm);
 		window.location.href = "http://localhost:8080/search?searchTerm=" + searchTerm;
 	}
+};
+
+$.fn.strech_text = function () {
+    var elmt = $(this),
+        cont_width = elmt.width(),
+        txt = elmt.html(),
+        one_line = $('<span class="stretch_it">' + txt + '</span>'),
+        nb_char = elmt.text().length,
+        spacing = cont_width / nb_char,
+        txt_width;
+
+    elmt.html(one_line);
+    txt_width = one_line.width();
+
+    if (txt_width < cont_width) {
+        var char_width = txt_width / nb_char,
+            ltr_spacing = spacing - char_width + (spacing - char_width) / nb_char;
+
+        one_line.css({
+            'letter-spacing': ltr_spacing
+        });
+    } else {
+        one_line.contents().unwrap();
+        elmt.addClass('justify');
+    }
 };
